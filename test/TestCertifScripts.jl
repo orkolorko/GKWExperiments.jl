fc13370d602e773377d159bee0163c6c17eb0735
Isaia Nisoli <nisoli@im.ufrj.br>
2025-10-08 10:43:09 -0300

    Add tests for certification utilities

---
+++ test/TestCertifScripts.jl
@@ -0,0 +1,48 @@
+using Test
+using LinearAlgebra
+using BallArithmetic
+using GKWExperiments.CertifScripts
+
+@testset "compute_schur_and_error" begin
+    A = BallArithmetic.BallMatrix([1 + 2im  0.2 - 0.1im; -0.3 + 0.05im  0.7 - 1.0im])
+
+    S, errF, errT, norm_Z, norm_Z_inv = compute_schur_and_error(A)
+
+    @test isa(S, LinearAlgebra.Schur)
+    @test isa(errF, BallArithmetic.Ball)
+    @test isa(errT, BallArithmetic.Ball)
+    @test isa(norm_Z, BallArithmetic.Ball)
+    @test isa(norm_Z_inv, BallArithmetic.Ball)
+
+    _, _, errT_identity, _, _ = compute_schur_and_error(A; polynomial = (0.0, 1.0))
+    @test errT_identity == errT
+
+    coeffs = (0.3, -0.4, 0.1)
+    S_poly, errF_poly, errT_poly, norm_Z_poly, norm_Z_inv_poly = compute_schur_and_error(A; polynomial = coeffs)
+
+    bZ = BallArithmetic.BallMatrix(S_poly.Z)
+    bT = BallArithmetic.BallMatrix(S_poly.T)
+
+    pA = CertifScripts._polynomial_matrix(coeffs, A)
+    pT = CertifScripts._polynomial_matrix(coeffs, bT)
+    manual_err = BallArithmetic.svd_bound_L2_opnorm(bZ * pT * bZ' - pA)
+
+    @test errF_poly == errF
+    @test errT_poly == manual_err
+    @test norm_Z_poly == norm_Z
+    @test norm_Z_inv_poly == norm_Z_inv
+end
+
+@testset "bound_res_original" begin
+    A = BallArithmetic.BallMatrix([1 + 2im  0.2 - 0.1im; -0.3 + 0.05im  0.7 - 1.0im])
+    _, _, errT, norm_Z, norm_Z_inv = compute_schur_and_error(A)
+
+    l2pseudo = BallArithmetic.Ball(1.1, 1e-3)
+    η = 0.25
+    N = size(A, 1)
+
+    bound = bound_res_original(l2pseudo, η, norm_Z, norm_Z_inv, errT, errT, N)
+
+    @test isa(bound, Float64)
+    @test bound > 0
+end
--- test/runtests.jl
+++ test/runtests.jl
@@ -4,4 +4,5 @@ using Test
 @testset "GKWExperiments.jl" begin
     include("TestZeta.jl")
     include("TestGKWConsistency.jl")
+    include("TestCertifScripts.jl")
 end
